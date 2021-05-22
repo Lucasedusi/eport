35916e64aa57b5d2cfd82ac2d928670fd4a0c9f4
Osvaldo Aurélio <osvaldo.aurelio@outlook.com>
2021-05-22 13:52:49 -0300

    Add redirect effect

--- frontend/src/pages/Welcome/index.jsx
+++ frontend/src/pages/Welcome/index.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Lottie from 'react-lottie';
 
 import animationData from '../../assets/animation.json';
 
 import * as STD from './styles';
+import { useEffect } from 'react';
 
 const options = {
   loop: true,
@@ -14,22 +15,36 @@ const options = {
   },
 };
 
-const Welcome = () => (
-  <STD.Container>
-    <STD.Title>
-      Trilhe novos caminhos <br /> como empreendedor
-    </STD.Title>
+const Welcome = () => {
+  const history = useHistory();
 
-    <Lottie options={options} height={275} width={300} />
+  useEffect(() => {
+    const redirect = setTimeout(() => {
+      history.push('/dashboard');
+    }, 3000);
 
-    <STD.SubTitle>
-      Olá, para todos os empreendedores sua jornada começa bem aqui!
-    </STD.SubTitle>
+    return () => {
+      clearTimeout(redirect);
+    }
+  }, [history]);
 
-    <STD.Button>
-      <Link to="/dashboard">Começar</Link>
-    </STD.Button>
-  </STD.Container>
-);
+  return (
+    <STD.Container>
+      <STD.Title>
+        Trilhe novos caminhos <br /> como empreendedor
+      </STD.Title>
+
+      <Lottie options={options} height={275} width={300} />
+
+      <STD.SubTitle>
+        Olá, para todos os empreendedores sua jornada começa bem aqui!
+      </STD.SubTitle>
+
+      <STD.Button>
+        <Link to="/dashboard">Começar</Link>
+      </STD.Button>
+    </STD.Container>
+  );
+};
 
 export default Welcome;
