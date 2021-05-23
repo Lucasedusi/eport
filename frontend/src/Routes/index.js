c3fda55a2332daa5690503fb5215d019ff7dc190
Lucasedusi <lucasmto.16@hotmail.com>
2021-05-23 08:59:03 -0300

    create trail page

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -5,20 +5,22 @@ import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Entrepreneur from '../pages/Entrepreneur';
 import Bidding from '../pages/Bidding';
+import Trail from '../pages/Trail';
 import BiddingSingle from '../pages/BiddingSingle';
 import NotFound from '../pages/NotFound';
 
 const Routes = () => (
   <Switch>
-    <Route path='/' exact component={Welcome} />
-    <Route path='/signin' component={SignIn} />
-    <Route path='/dashboard' component={Dashboard} />
-    <Route path='/entrepreneur' component={Entrepreneur} />
-    <Route path='/bidding' component={Bidding} />
-    <Route path='/biddingSingle' component={BiddingSingle} />
-    <Route path='/bidding' component={Bidding} />
+    <Route path="/" exact component={Welcome} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/entrepreneur" component={Entrepreneur} />
+    <Route path="/bidding" component={Bidding} />
+    <Route path="/biddingSingle" component={BiddingSingle} />
+    <Route path="/bidding" component={Bidding} />
+    <Route path="/trail" component={Trail} />
 
-    <Route path='*' component={NotFound} />
+    <Route path="*" component={NotFound} />
   </Switch>
 );
 
---
+++ frontend/src/pages/Trail/styles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #fff;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+export const Title = styled.h2`
+  font-weight: normal;
+  font-size: 14px;
+  color: #666666;
+`;
+
+export const Light = styled.img``;
+
+export const Help = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 20px;
+`;
+
+export const TitleHelp = styled.h2`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+  color: #253368;
+`;
+
+export const BoxMain = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding: 15px;
+`;
+export const BoxChildrenLeft = styled.div`
+  background-color: antiquewhite;
+  padding: 30px;
+`;
+
+export const BoxChildrenRight = styled.div``;
