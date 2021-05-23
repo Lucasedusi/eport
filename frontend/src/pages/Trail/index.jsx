e7a32bdeddda6d619f0bbf492566398a7560b3f1
Lucasedusi <lucasmto.16@hotmail.com>
2021-05-23 10:00:44 -0300

    refactory  trail page

--- frontend/src/pages/Trail/index.jsx
+++ frontend/src/pages/Trail/index.jsx
@@ -14,9 +14,46 @@ const Bidding = () => (
       <STD.TitleHelp>Formalizar seu empreendimento é algo que pode ser feito pela internet!</STD.TitleHelp>
     </STD.Help>
 
+    <STD.Header>
+      <STD.TitleHeader>
+        Se você ainda não é MEI (Micro Empreendedor Individual) ou ME (Micro Empresa) e não faz ideia de onde se
+        enquadra então verifique nosso processo decisor
+      </STD.TitleHeader>
+      <STD.Light src={people} />
+    </STD.Header>
+
+    <STD.BoxInitial>
+      <STD.BoxText>
+        Você é o único funcionário da empresa, ou tem no máximo mais uma pessoa trabalhando com você?
+      </STD.BoxText>
+    </STD.BoxInitial>
+
     <STD.BoxMain>
-      <STD.BoxChildrenLeft>TESTE</STD.BoxChildrenLeft>
-      <STD.BoxChildrenRight>YESTE</STD.BoxChildrenRight>
+      <STD.BoxChildrenLeft>
+        Seu rendimento anual é maior do que R$81.000,00? Ou poderá chegar a ser?
+      </STD.BoxChildrenLeft>
+    </STD.BoxMain>
+
+    <STD.BoxMain>
+      <STD.BoxChildrenLeft>Você pretende contratar mais funcionários durante esse ano?</STD.BoxChildrenLeft>
+    </STD.BoxMain>
+
+    <STD.BoxChildren>
+      <STD.BoxChildrenRight>
+        Seu empreendimento se enquadra como Micro Empresa. Para formalizar entre em contato com a Sala do Empreendedor{' '}
+        <br />
+        (63) 3411-7026
+      </STD.BoxChildrenRight>
+    </STD.BoxChildren>
+
+    <STD.BoxMain>
+      <STD.BoxChildrenLeft>
+        Seu empreendimento se enquadra como MEI. A abertura pode ser feita online e gratuitamente. <br />
+        <br />
+        <a href="https://www.gov.br/pt-br/servicos/realizar-registro-como-microempreendedor-individual-mei">
+          https://www.gov.br/pt-br/servicos/realizar-registro-como-microempreendedor-individual-mei
+        </a>
+      </STD.BoxChildrenLeft>
     </STD.BoxMain>
   </STD.Container>
 );
